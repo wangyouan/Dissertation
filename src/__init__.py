0dd5972d763bbb91971bbd60d341612667bea5cd
Mark Wang <wangyouan0629@hotmail.com>
2016-04-08 15:25:29 +0800

    Add file dependency for pyspark

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: Dissertation
+# File name: __init__.py
+# Author: Mark Wang
+# Date: 8/4/2016
---
+++ src/mean_average_algorithm.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: Dissertation
+# File name: mean_average_algorithm
+# Author: Mark Wang
+# Date: 8/4/2016
