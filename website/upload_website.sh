f15a1c82ad95d24c6eaec2ee17540821fa73123d
Mark Wang <wangyouan0629@hotmail.com>
2016-04-27 10:35:18 +0800

    Add new shell script to upload website file to remote server

---
+++ website/upload_website.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Compress all Html files"
+tar -zcf website.tar.gz *
+
+echo "Removing unused files"
+sshpass -f ../password.txt ssh msd15090@i.cs.hku.hk "rm -rf public_html/*"
+
+echo "Upload compress files"
+sshpass -f ../password.txt scp website.tar.gz msd15090@i.cs.hku.hk:public_html/website.tar.gz
+
+echo "Untar all files in the server"
+sshpass -f ../password.txt ssh msd15090@i.cs.hku.hk "tar -zxf ~/public_html/website.tar.gz -C ~/public_html/"
+
+echo "Remove temporary files from website"
+sshpass -f ../password.txt ssh msd15090@i.cs.hku.hk "rm public_html/website.tar.gz"
+
+echo "remove temporary files"
+rm website.tar.gz
