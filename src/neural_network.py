468550bf357a4e606c289575ffc7ef842d2f8824
Mark Wang <wangyouan0629@hotmail.com>
2016-05-05 19:56:18 +0800

    Add final report sample Add neural network class

---
+++ src/neural_network.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: Dissertation
+# File name: neural_network
+# Author: Mark Wang
+# Date: 5/5/2016
+
+
+class NeuralNetwork(object):
+    def __init__(self, layers, seed=None):
+        pass
+
+    def train(self, data, learn_rate=0.5, iteration=100, error=1e-8):
+        pass
+
+    def predict(self, features):
+        pass
