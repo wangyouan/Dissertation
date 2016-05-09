7fe1eb18c7740364de8c40e121b9dfe141ca977d
Mark Wang <wangyouan0629@hotmail.com>
2016-05-09 22:55:46 +0800

    Change submit files

--- start_spark.sh
+++ start_spark.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 spark-submit --master "spark://student32-x1:7077" \
     --py-files dist/StockSimulator-0.1-py2.7.egg \
-    --driver-memory	1g\
-    --executor-memory 1g\
+    --driver-memory	1g \
+    --executor-memory 1g \
+    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:~/src/dl4j-spark-cdh5-examples/src/main/resources/log4j.properties" \
     src/test_distributed_neural_network.py
