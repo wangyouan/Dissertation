43d7c5489f3e08c94353bf9d079242ea6ea3cb29
Mark Wang <wangyouan0629@hotmail.com>
2016-05-09 22:58:56 +0800

    Add log4g properties

--- start_spark.sh
+++ start_spark.sh
@@ -3,5 +3,5 @@ spark-submit --master "spark://student32-x1:7077" \
     --py-files dist/StockSimulator-0.1-py2.7.egg \
     --driver-memory	1g \
     --executor-memory 1g \
-    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:~/src/dl4j-spark-cdh5-examples/src/main/resources/log4j.properties" \
+    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/test_distributed_neural_network.py
