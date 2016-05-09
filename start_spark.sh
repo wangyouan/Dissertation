23748e94bffede0f86b50dcd2ec9b05510d7562d
Mark Wang <wangyouan0629@hotmail.com>
2016-05-09 21:41:16 +0800

    Fix bugs about start_spark error

--- start_spark.sh
+++ start_spark.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spark-submit --master "spark://stduent32-x1:7077" --py-files dist/StockSimulator-0.1-py2.7.egg StockSimulator/RegressionMethod/distributed_neural_network.py
+spark-submit --master "spark://student32-x1:7077" --py-files dist/StockSimulator-0.1-py2.7.egg StockSimulator/RegressionMethod/distributed_neural_network.py
