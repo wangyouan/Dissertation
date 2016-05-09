b9787aa98ba3618925e2183cb597936769ae651f
Mark Wang <wangyouan0629@hotmail.com>
2016-05-09 22:08:10 +0800

    Update run info

--- start_spark.sh
+++ start_spark.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-spark-submit --master "spark://student32-x1:7077" --py-files dist/StockSimulator-0.1-py2.7.egg StockSimulator/RegressionMethod/distributed_neural_network.py
+spark-submit --master "spark://student32-x1:7077" \
+    --py-files dist/StockSimulator-0.1-py2.7.egg \
+    --driver-memory	1g\
+    --executor-memory 1g\
+    StockSimulator/RegressionMethod/distributed_neural_network.py
