1d75176e99cd5832820d2d754f6db6690c32395d
Mark Wang <wangyouan0629@hotmail.com>
2016-06-12 13:49:57 +0800

    Fix an error in start-spark

--- start_spark.sh
+++ start_spark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 spark-submit --master "spark://Master:7077" \
-    --py-files dist/StockInference-0.1-py2.7.egg \
+    --py-files dist/StockInference-0.2-py2.7.egg \
     --driver-memory	1g \
     --executor-memory 2g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
