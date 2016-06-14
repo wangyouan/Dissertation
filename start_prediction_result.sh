8d7346961845779b744acb9c7c285b69e45bcfce
Mark Wang <wangyouan0629@hotmail.com>
2016-06-14 11:31:50 +0800

    Fix bugs about price prediction

--- start_prediction_result.sh
+++ start_prediction_result.sh
@@ -2,27 +2,28 @@
 
 echo $#
 
-if [ $# -eq 0 ]
+if [ $# -eq 1 ]
 then
+    echo $1
     spark-submit --master "spark://Master:7077" \
         --py-files dist/StockInference-0.2-py2.7.egg \
         --driver-memory	1g \
         --executor-memory 2g \
         --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
         src/predict_price.py
-elif [ $# -eq 1 ]
+elif [ $# -eq 2 ]
 then
     spark-submit --master "spark://Master:7077" \
         --py-files dist/StockInference-0.2-py2.7.egg \
         --driver-memory	1g \
         --executor-memory 2g \
         --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-        src/predict_price.py $1
+        src/predict_price.py $2
 else
     spark-submit --master "spark://Master:7077" \
         --py-files dist/StockInference-0.2-py2.7.egg \
         --driver-memory	1g \
         --executor-memory 2g \
         --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-        src/predict_price.py $1 $2
+        src/predict_price.py $2 $3
 fi
