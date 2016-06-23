d2d42f9b83229127fc8a8de7128dd0ecf456ce79
Mark Wang <wangyouan0629@hotmail.com>
2016-06-23 22:43:20 +0800

    test both stock price at the same time

--- start_spark.sh
+++ start_spark.sh
@@ -5,3 +5,10 @@ spark-submit --master "spark://Master:7077" \
     --executor-memory 2g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/test_combine_system.py
+
+spark-submit --master "spark://Master:7077" \
+    --py-files dist/StockInference-0.3-py2.7.egg \
+    --driver-memory	1g \
+    --executor-memory 2g \
+    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
+    src/stock_prediction.py
