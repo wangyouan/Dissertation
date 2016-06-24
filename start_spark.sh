57e1f5e6ff3b375998110d4ca2181062c9797f0f
Mark Wang <wangyouan0629@hotmail.com>
2016-06-24 10:59:26 +0800

    test another 5 years

--- start_spark.sh
+++ start_spark.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#spark-submit --master "spark://Master:7077" \
-#    --py-files dist/StockInference-0.3-py2.7.egg \
-#    --driver-memory	1g \
-#    --executor-memory 2g \
-#    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-#    src/test_combine_system.py
+spark-submit --master "spark://Master:7077" \
+    --py-files dist/StockInference-0.3-py2.7.egg \
+    --driver-memory	1g \
+    --executor-memory 2g \
+    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
+    src/test_combine_system.py
 
 spark-submit --master "spark://Master:7077" \
     --py-files dist/StockInference-0.3-py2.7.egg \
