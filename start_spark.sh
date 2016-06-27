788c7cf8fc368a4932c7d5b24169d47130318439
Mark Wang <wangyouan0629@hotmail.com>
2016-06-27 14:32:55 +0800

    automatic zip

--- start_spark.sh
+++ start_spark.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-start_date="2012-12-29"
+start_date="2013-01-01"
 end_date="2015-01-06"
 test_ratio=0.5
+
+rm -rf output/*
 
 spark-submit --master "spark://Master:7077" \
     --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
@@ -17,3 +19,9 @@ spark-submit --master "spark://Master:7077" \
     --executor-memory 2g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/stock_prediction.py $start_date $end_date $test_ratio
+
+
+file_name=output_${start_date:0:4}_${end_date:0:4}.tar.gz
+
+tar -zcf $file_name output/
+mv $file_name ../output/
