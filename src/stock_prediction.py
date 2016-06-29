ffa51b1e70ac48bd46b992329121c7f8f1ee8ab0
Mark Wang <wangyouan0629@hotmail.com>
2016-06-29 19:45:52 +0800

    Change method list

--- src/stock_prediction.py
+++ src/stock_prediction.py
@@ -92,7 +92,7 @@ stock_list = ['0001.HK', '0002.HK', '0003.HK', '0004.HK', '0005.HK', '0006.HK', 
               '0066.HK', '1123.HK']
 
 test = None
-for method in [const.ARTIFICIAL_NEURAL_NETWORK, const.RANDOM_FOREST, const.LINEAR_REGRESSION]:
+for method in [const.RANDOM_FOREST, const.LINEAR_REGRESSION, const.ARTIFICIAL_NEURAL_NETWORK]:
 
     new_file_path = os.path.join(output_path, method.lower())
     if not os.path.isdir(new_file_path):
