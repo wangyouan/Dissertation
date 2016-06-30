3386c953180fb86e8710fa60de40e4565fe4813e
Mark Wang <wangyouan0629@hotmail.com>
2016-06-30 21:40:28 +0800

    change usage type

--- start_spark.sh
+++ start_spark.sh
@@ -11,17 +11,17 @@ spark-submit --master "spark://Master:7077" \
     --driver-memory	1g \
     --executor-memory 2g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-    src/test_combine_system.py $start_date $end_date $test_ratio
+    src/test_combine_system.py ${start_date} ${end_date} ${test_ratio}
 
 spark-submit --master "spark://Master:7077" \
     --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
     --driver-memory	1g \
     --executor-memory 2g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-    src/stock_prediction.py $start_date $end_date $test_ratio
+    src/stock_prediction.py ${start_date} ${end_date} ${test_ratio}
 
 
 file_name=output_3_1_${start_date:0:4}_${end_date:0:4}.tar.gz
 
-tar -zcf $file_name output/
-mv $file_name ../output/
+tar -zcf ${file_name} output/
+mv ${file_name} ../output/
