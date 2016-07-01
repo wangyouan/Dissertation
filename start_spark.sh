6ae06f4a52d8b643d316f1e450fdc5c578c7c477
Mark Wang <wangyouan0629@hotmail.com>
2016-07-01 10:53:43 +0800

    Change date to start_date="2009-01-06" end_date="2012-01-06" test_ratio="2011-01-06"

--- start_spark.sh
+++ start_spark.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-start_date="2013-01-06"
-end_date="2016-01-06"
-test_ratio="2015-01-06"
+start_date="2009-01-06"
+end_date="2012-01-06"
+test_ratio="2011-01-06"
 
 rm -rf output/*
 
@@ -21,7 +21,7 @@ spark-submit --master "spark://Master:7077" \
     src/stock_prediction.py ${start_date} ${end_date} ${test_ratio}
 
 
-file_name=output_3_1_${start_date:0:4}_${end_date:0:4}.tar.gz
+file_name=output_1_2_${start_date:0:4}_${end_date:0:4}.tar.gz
 
 tar -zcf ${file_name} output/
 mv ${file_name} ../output/
