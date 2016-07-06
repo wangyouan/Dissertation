b8b21a41ce261c114ce1c4b49546a580a027e17c
Mark Wang <wangyouan0629@hotmail.com>
2016-07-06 09:31:46 +0800

    Add the other two method testing result

--- start_spark.sh
+++ start_spark.sh
@@ -4,14 +4,14 @@ start_date="2013-01-06"
 end_date="2016-01-06"
 test_ratio="2015-01-06"
 
-rm -rf output/*
-
-spark-submit --master "spark://Master:7077" \
-    --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
-    --driver-memory	1g \
-    --executor-memory 2g \
-    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
-    src/test_combine_system.py ${start_date} ${end_date} ${test_ratio}
+#rm -rf output/*
+#
+#spark-submit --master "spark://Master:7077" \
+#    --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
+#    --driver-memory	1g \
+#    --executor-memory 2g \
+#    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
+#    src/test_combine_system.py ${start_date} ${end_date} ${test_ratio}
 
 spark-submit --master "spark://Master:7077" \
     --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
