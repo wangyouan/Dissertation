34cb7cd5b4bc32ac1447eb4013f07057ab11cab9
Mark Wang <wangyouan0629@hotmail.com>
2016-10-23 01:21:55 +0800

    fix a small bug

--- src/test_forecaster/test_all_method.py
+++ src/test_forecaster/test_all_method.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
              SF.CHANGE_DIRECTION: SF.ARTIFICIAL_NEURAL_NETWORK},
             SF.ARTIFICIAL_NEURAL_NETWORK, SF.LINEAR_REGRESSION, SF.RANDOM_FOREST,
         ]:
-            print train_method
+            # print train_method
             if isinstance(train_method, dict):
                 current_result_path = os.path.join(result_path, str(window_size),
                                                    "{}_{}".format(short_name_dict[train_method[SF.CHANGE_DIRECTION]],
@@ -72,36 +72,36 @@ if __name__ == '__main__':
             if not os.path.isdir(current_result_path):
                 os.makedirs(current_result_path)
 
-                for i in range(len(hsi_stock_list)):
-                    start_time = time.time()
-                    stock = hsi_stock_list[i]
-                    print 'start to get stock', stock
-                    save_file_name = '{}_{}_{}.csv'.format(stock[:4], short_name_dict.get(train_method), window_size)
+            for i in range(len(hsi_stock_list)):
+                start_time = time.time()
+                stock = hsi_stock_list[i]
+                print 'start to get stock', stock
+                save_file_name = '{}_{}_{}.csv'.format(stock[:4], short_name_dict.get(train_method), window_size)
 
-                    try:
+                try:
 
-                        result = predict_stock_price_spark(stock_symbol=stock, data_path=data_path,
-                                                           worker_num=worker_number,
-                                                           train_method=train_method, start_date=start_date,
-                                                           end_date=end_date,
-                                                           test_date=test_date, window_size=window_size)
-                    except Exception, err:
-                        import traceback
+                    result = predict_stock_price_spark(stock_symbol=stock, data_path=data_path,
+                                                       worker_num=worker_number,
+                                                       train_method=train_method, start_date=start_date,
+                                                       end_date=end_date,
+                                                       test_date=test_date, window_size=window_size)
+                except Exception, err:
+                    import traceback
 
-                        traceback.print_exc()
-                        print stock
-                        break
+                    traceback.print_exc()
+                    print stock
+                    break
 
-                    else:
-                        result[['Target', 'TodayPrice', 'prediction']].to_csv(
-                            os.path.join(current_result_path, save_file_name))
+                else:
+                    result[['Target', 'TodayPrice', 'prediction']].to_csv(
+                        os.path.join(current_result_path, save_file_name))
 
-                        df.loc[i] = {
-                            'sdpr': calculate_success_direction_prediction_rate(result, SF.TODAY_PRICE, 'prediction',
-                                                                                SF.TARGET_PRICE),
-                            'mse': calculate_mean_squared_error(result, 'prediction', SF.TARGET_PRICE),
-                            'mape': calculate_mean_absolute_percentage_error(result, 'prediction', SF.TARGET_PRICE),
-                            'stock': stock,
-                            'time': time.time() - start_time}
+                    df.loc[i] = {
+                        'sdpr': calculate_success_direction_prediction_rate(result, SF.TODAY_PRICE, 'prediction',
+                                                                            SF.TARGET_PRICE),
+                        'mse': calculate_mean_squared_error(result, 'prediction', SF.TARGET_PRICE),
+                        'mape': calculate_mean_absolute_percentage_error(result, 'prediction', SF.TARGET_PRICE),
+                        'stock': stock,
+                        'time': time.time() - start_time}
 
-                df.to_csv(os.path.join(current_result_path, 'statistics.csv'), index=False)
+            df.to_csv(os.path.join(current_result_path, 'statistics.csv'), index=False)
