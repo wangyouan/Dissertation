9cfc697954692f0ff7c7d3c9230ae96246d60e80
Mark Wang <wangyouan0629@hotmail.com>
2016-10-23 15:12:05 +0800

    Change setting

--- start_test_ann_layers.sh
+++ start_test_ann_layers.sh
@@ -7,5 +7,4 @@ spark-submit --master local[${WORKER_NUM}] \
     --py-files dist/${PACKAGE_NAME} \
     --driver-memory	1g \
     --executor-memory 1g \
-    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/test_forecaster/test_ann_classifier.py
