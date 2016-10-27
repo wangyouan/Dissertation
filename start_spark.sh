5cbff4c810f206bb99accfe67959d5788f0a3b04
Mark Wang <wangyouan0629@hotmail.com>
2016-10-27 14:20:13 +0800

    change some file

--- start_spark.sh
+++ start_spark.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-start_date="2011-01-06"
+start_date="2013-01-06"
 end_date="2015-01-06"
 test_ratio="2014-01-06"
 
 rm -rf output/*
 
-spark-submit --master "spark://Master:7077" \
+spark-submit --master "local[*]" \
     --py-files dist/StockInference-0.3-py2.7.egg,src/__init__.py \
     --driver-memory	1g \
-    --executor-memory 2g \
+    --executor-memory 1g \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/test_combine_system.py ${start_date} ${end_date} ${test_ratio}
 #
