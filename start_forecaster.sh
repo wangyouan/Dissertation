17650176fec183eb20a544737634e8925104a249
Mark Wang <wangyouan0629@hotmail.com>
2016-11-01 23:02:36 +0800

    fix a bug about adx

--- start_forecaster.sh
+++ start_forecaster.sh
@@ -3,9 +3,13 @@
 WORKER_NUM=2
 PACKAGE_NAME=stockforecaster-0.1-py2.7.egg
 
-spark-submit --master local[${WORKER_NUM}] \
-    --py-files dist/${PACKAGE_NAME} \
-    --driver-memory	1g \
-    --executor-memory 1g \
+#spark-submit \
+#    --master local[${WORKER_NUM}] \
+#    --py-files dist/${PACKAGE_NAME} \
+#    --driver-memory	1g \
+#    --executor-memory 1g \
+#    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
+#    src/test_forecaster/test_all_method.py
+spark-submit --py-files dist/${PACKAGE_NAME} \
     --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./logs/log4j.properties" \
     src/test_forecaster/test_all_method.py
